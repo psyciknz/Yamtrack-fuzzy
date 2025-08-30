6bbcb1f41fb18859df99ef108921bfe2510ea993
DannyVFilms <dannyvfilms@gmail.com>
2025-08-29 21:36:13 -0500

    fix: resolve merge conflict; use HomeSortChoices keys once

--- src/app/models.py
+++ src/app/models.py
@@ -486,14 +486,13 @@ class MediaManager(models.Manager):
         """Sort in-progress media based on the sort criteria."""
         # Define primary sort functions based on sort_by
         primary_sort_functions = {
-            users.models.HomeSortChoices.RECENT: lambda x: -timezone.datetime.timestamp(
-                x.progressed_at if x.progressed_at is not None else x.created_at,
-            ),
             users.models.HomeSortChoices.UPCOMING: lambda x: (
                 x.next_event is None,
                 x.next_event.datetime if x.next_event else None,
             ),
-            users.models.HomeSortChoices.TITLE: lambda x: x.item.title.lower(),
+            users.models.HomeSortChoices.RECENT: lambda x: -timezone.datetime.timestamp(
+                x.progressed_at if x.progressed_at is not None else x.created_at,
+            ),
             users.models.HomeSortChoices.COMPLETION: lambda x: (
                 x.max_progress is None,
                 -(
@@ -506,6 +505,7 @@ class MediaManager(models.Manager):
                 x.max_progress is None,
                 (x.max_progress - x.progress if x.max_progress else 0),
             ),
+            users.models.HomeSortChoices.TITLE: lambda x: x.item.title.lower(),
         }
 
         primary_sort_function = primary_sort_functions[sort_by]
