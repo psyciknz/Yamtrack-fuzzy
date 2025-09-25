6b2cbd63ae1fe9beb3572f085d23d36a5cfd9f03
psyciknz <github@andc.co.nz>
2025-09-25 21:07:07 +1200

    Cleaned up additions to the models when were not needed.

--- src/app/history.py
+++ src/app/history.py
@@ -20,36 +20,6 @@ logger = logging.getLogger(__name__)
 from .models import Episode, Item
 
 
-def record_watch_history(user, item, episode=None, rewatch=False, progress_minutes=None):
-    """Record a watch history entry."""
-    return WatchHistory.objects.create(
-        user=user,
-        item=item,
-        episode=episode,
-        rewatch=rewatch,
-        progress_minutes=progress_minutes
-    )
-
-def get_user_watch_stats(user, days=None):
-    """Get user's watch statistics."""
-    temp_qs = WatchHistory.objects.all()
-    print(temp_qs.count())
-    qs = WatchHistory.objects.filter(user=user)
-    
-    if days:
-        from datetime import timedelta
-
-        from django.utils import timezone
-        cutoff = timezone.now() - timedelta(days=days)
-        qs = qs.filter(watched_date__gte=cutoff)
-    
-    return {
-        'total': qs.count(),
-        'movies': qs.filter(item__media_type='movie').count(),
-        'episodes': qs.filter(item__media_type__in=['tv', 'anime']).count(),
-        'unique_items': qs.values('item').distinct().count(),
-    }
-    
 def get_user_consumable_media(user, start_date=None, end_date=None):
     """
     Get all individual consumable media items (movies, episodes, etc.) for a user.
